9642a19619ec61cf99574f4a389d7e7f4388ec26
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-08-09 22:28:44 +0800

    module_utils: use patch command
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/utils/module_utils.sh
+++ scripts/utils/module_utils.sh
@@ -80,7 +80,7 @@ APPLY_PATCH()
     DECODE_APK "$PARTITION" "$FILE" || return 1
 
     LOG "- Applying \"$(grep "^Subject:" "$PATCH" | sed "s/.*PATCH] //; s/.*PATCH .\/.] //")\" to /$PARTITION/$FILE"
-    EVAL "LC_ALL=C git apply --directory=\"$APKTOOL_DIR/$PARTITION/${FILE//system\//}\" --verbose --unsafe-paths \"$PATCH\"" || return 1
+    EVAL "LC_ALL=C patch -p1 --no-backup-if-mismatch -d \"$APKTOOL_DIR/$PARTITION/${FILE//system\//}\" < \"$PATCH\"" || return 1
 }
 
 # DECODE_APK <partition> <apk/jar>
